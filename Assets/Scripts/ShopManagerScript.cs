90aa673304785ba2d192b37b5ea1ec15c2801bfe
Colin Song <colinsong8090@gmail.com>
2025-01-13 19:26:32 -0500

    deleted line

--- Assets/Scripts/ShopManagerScript.cs
+++ Assets/Scripts/ShopManagerScript.cs
@@ -85,7 +85,7 @@ public class ShopManagerScript : MonoBehaviour
                 }
             }
             ifSpinSuccessful = false;
-            OpenGachaWindow()l
+            
             OnGachaAnimationComplete();
         }
     }
